4303f95bc29d276797caecce3a071cbddc54cb40
RyanTseng <ryan10328@gmail.com>
2018-08-10 20:06:34 +0800

    first commit

---
+++ TennisGameTdd/TennisGameTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TennisGameTdd
+{
+    [TestClass]
+    public class TennisGameTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
