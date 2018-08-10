4536300cb3d7d3b933181e32487ebb3a3be1c767
RyanTseng <ryan10328@gmail.com>
2018-08-10 20:13:15 +0800

    pass test: FifteenLove

--- TennisGameTdd/TennisGameTests.cs
+++ TennisGameTdd/TennisGameTests.cs
@@ -12,13 +12,33 @@ namespace TennisGameTdd
             var actual = sut.Score();
             Assert.AreEqual("Love All", actual);
         }
+        
+        [TestMethod]
+        public void FifteenLove()
+        {
+            var sut = new TennisGame();
+            sut.HomePlayerScored();
+            var actual = sut.Score();
+            Assert.AreEqual("Fifteen Love", actual);
+        }
     }
 
     public class TennisGame
     {
+        private int _homePlayerScore;
+
         public string Score()
         {
+            if (_homePlayerScore == 1)
+            {
+                return "Fifteen Love";
+            }
             return "Love All";
+        }
+
+        public void HomePlayerScored()
+        {
+            _homePlayerScore++;
         }
     }
 }
